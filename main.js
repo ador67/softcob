d159f6537488b13c737f3945664fb63f7fed47ee
ador67 <abuhanifsagor72@gmail.com>
2021-09-02 18:09:48 +0600

    all done

--- main.js
+++ main.js
@@ -7,5 +7,23 @@ function settingMenuToggle(){
 darkMode.onclick = function(){
   darkMode.classList.toggle("dark-mode-on");
   document.body.classList.toggle("dark-theam")
+  
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+    }
 }
 
+if(localStorage.getItem("theme") == "light"){
+    darkMode.classList.remove("dark-mode-on");
+    document.body.classList.remove("dark-theam");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkMode.classList.add("dark-mode-on");
+    document.body.classList.add("dark-theam");
+}
+else{
+  localStorage.setItem("theme","light");
+}
